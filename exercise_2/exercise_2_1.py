2838b9d10543f381e357bbae6f75d35a74bace28
Karoline T. Dutra <karolktd@gmail.com>
2026-04-28 09:52:35 +0200

    tutorial test to commit in github

---
+++ exercise_2/exercise_2_1.py
@@ -0,0 +1,15 @@
+#exercise_2_1.py
+#Author: Karoline Trindade Dutra
+#Date: 2026-04-28
+#Description: Exercise 2.1 - Three basic Python functions 
+#             for the Python in Qgis and ArcGis Course
+
+# IT IS JUST A TEST, DO NOT CONSIDER THE CODE BELOW.
+
+def greet(name):
+    message = "Hello, " + name + "!"
+    return message
+
+print(greet("Alice"))
+print(greet("Karol"))
+
